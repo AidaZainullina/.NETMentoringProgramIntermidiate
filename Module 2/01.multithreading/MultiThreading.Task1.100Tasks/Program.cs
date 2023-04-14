d332d6b8b09d801ae21527f647edc6c3f0a12278
MOSCOW\Aida_Zainullina <aida_zainullina@epam.com>
2023-04-14 11:01:57 +0200

    module 2 - implementation of the task 1 was added

--- Module 2/01.multithreading/MultiThreading.Task1.100Tasks/Program.cs
+++ Module 2/01.multithreading/MultiThreading.Task1.100Tasks/Program.cs
@@ -4,35 +4,48 @@
  * “Task #0 – {iteration number}”.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task1._100Tasks
 {
-    class Program
-    {
-        const int TaskAmount = 100;
-        const int MaxIterationsCount = 1000;
+	class Program
+	{
+		const int TaskAmount = 100;
+		const int MaxIterationsCount = 1000;
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine(".Net Mentoring Program. Multi threading V1.");
-            Console.WriteLine("1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.");
-            Console.WriteLine("Each Task should iterate from 1 to 1000 and print into the console the following string:");
-            Console.WriteLine("“Task #0 – {iteration number}”.");
-            Console.WriteLine();
-            
-            HundredTasks();
+		static void Main(string[] args)
+		{
+			Console.WriteLine(".Net Mentoring Program. Multi threading V1.");
+			Console.WriteLine("1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.");
+			Console.WriteLine("Each Task should iterate from 1 to 1000 and print into the console the following string:");
+			Console.WriteLine("“Task #0 – {iteration number}”.");
+			Console.WriteLine();
 
-            Console.ReadLine();
-        }
+			HundredTasks();
 
-        static void HundredTasks()
-        {
-            // feel free to add your code here
-        }
+			Console.ReadLine();
+		}
 
-        static void Output(int taskNumber, int iterationNumber)
-        {
-            Console.WriteLine($"Task #{taskNumber} – {iterationNumber}");
-        }
-    }
+		static void HundredTasks()
+		{
+			var taskList = new List<Task>();
+			for (var i = 0; i < TaskAmount; i++)
+			{
+				for (var j = 0; j < MaxIterationsCount; j++)
+				{
+					var lastTask = new Task(() => Output(i, j));
+					lastTask.Start();
+					taskList.Add(lastTask);
+				}
+			}
+
+			Task.WaitAll(taskList.ToArray());
+		}
+
+		static void Output(int taskNumber, int iterationNumber)
+		{
+			Console.WriteLine($"Task #{taskNumber} – {iterationNumber}");
+		}
+	}
 }
